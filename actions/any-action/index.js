b58e49e4793db9c208b555d701a4ce4328e27cc4
Jinzheng Sha <jsha@linkedin.com>
2019-10-09 22:21:19 -0700

    Add any-action

---
+++ actions/any-action/index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  console.log(`This is the log for any-action`);
+
+  const anyinput = core.getInput('anyinput')
+
+  core.setOutput("anyoutput", anyinput);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
